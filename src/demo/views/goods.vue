6613e76034e6c4e31a3180a9608dcd16aa4751e9
闵翔 <546143220@qq.com>
2023-05-23 18:03:00 +0800

    feat: 新增创建文件及内容命令

---
+++ src/demo/views/goods.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <cl-crud ref="Crud">
+      <cl-row>
+        <!-- 刷新按钮 -->
+        <cl-refresh-btn />
+        <!-- 新增按钮 -->
+
+        <cl-add-btn />
+        <!-- 删除按钮 -->
+
+        <cl-multi-delete-btn />
+        <cl-flex1 />
+        <!-- 关键字搜索 -->
+        <cl-search-key />
+      </cl-row>
+
+      <cl-row>
+        <!-- 数据表格 -->
+        <cl-table ref="Table" />
+      </cl-row>
+
+      <cl-row>
+        <cl-flex1 />
+        <!-- 分页控件 -->
+        <cl-pagination />
+      </cl-row>
+
+      <!-- 新增、编辑 -->
+      <cl-upsert ref="Upsert" />
+    </cl-crud>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
